763a7beefd5bea9ea80f4cbbea5c6802f6ae8817
BrunoSantoni <brunosantoni98@gmail.com>
2021-07-19 00:43:05 -0300

    refactor(NavItem): Removida renderização condicional desnecessária

--- src/components/shared/Aside/NavItem/index.tsx
+++ src/components/shared/Aside/NavItem/index.tsx
@@ -17,18 +17,12 @@ export function NavItem({ icon, title, link }: NavItemProps) {
     isActive = true;
   }
 
-  // Caso seja o link selecionado atualmente, muda a estilização
-  if (isActive) {
-    return (
-      <li className="flex items-center text-yellow-300 justify-center gap-1 text-lg translate-x-0 transition color duration-200 lg:text-xl">
-        {icon}
-        <Link href={link}>{title}</Link>
-      </li>
-    );
-  }
-
   return (
-    <li className="flex items-center justify-center gap-1 text-lg translate-x-0 transition color duration-200 hover:text-yellow-100 lg:text-xl">
+    <li
+      className={`flex items-center justify-center gap-1 text-lg translate-x-0 transition color duration-200 hover:text-yellow-100 lg:text-xl ${
+        isActive && 'text-yellow-300'
+      }`}
+    >
       {icon}
       <Link href={link}>{title}</Link>
     </li>
